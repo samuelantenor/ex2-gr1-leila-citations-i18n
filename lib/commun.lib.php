6b892a5bc4ca1ad31a6d05cab3ca07f2e28702f5
Samuel Antenor <asamuelandre@gmail.com>
2022-10-05 17:01:21 -0400

    Correction du bogue de fichier introuvable pour les langues qui n'ont pas de citation.

--- lib/commun.lib.php
+++ lib/commun.lib.php
@@ -11,7 +11,14 @@
  */
 function obtenirCitationAleatoire($section, $langue)
 {
+    //vérifier si le fichier existe avant de l'utiliser
+    if(file_exists("data/citations-$section-$langue.json")){
     $citationsJson = file_get_contents("data/citations-$section-$langue.json");
+    }
+    else
+    {
+    $citationsJson = file_get_contents("data/citations-$section-fr.json");   
+    }
     $citations = json_decode($citationsJson, true);
     $positionAleatoire = rand(0, count($citations)-1);
     $citation = $citations[$positionAleatoire];
